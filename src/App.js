2ac1f6c2f181b767e86d88af76aaeaad1eb1444a
Jordan Morales <jordan.morales@gmail.com>
2019-12-20 12:52:04 -0800

    test run deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+// let proxyURL = 'https://cors-anywhere.herokuapp.com/'
+let charactersAPI = 'http://localhost:3010/characters/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: []
+    }
+  }
+
+  // pullCharacters = () => {
+  //   fetch('http://localhost:3010/characters/', {
+  //     mode: 'no-cors'
+  //   })
+  //   .then(response => console.log(JSON.stringify(response, null, 4)))
+  //   .then(json => console.log(json))
+  // }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      const characters = res.data;
+      this.setState({
+        characters: characters
+      })
+    })
+  }
+
+  render(){
+    return(
+      <div>
+          <ul>
+        { this.state.characters.map(character => <li>{character.name}</li>)}
+      </ul>
+      </div>
+    )
+  }
+
 }
+
+
 
 export default App;
